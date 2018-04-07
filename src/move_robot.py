43bb6ce22c6c11c1bed663f53fe6c1dc4ba313f9
valmik <rsnorunt@gmail.com>
2018-04-07 07:28:57 -0700

    not sure

--- src/move_robot.py
+++ src/move_robot.py
@@ -5,6 +5,7 @@ import moveit_commander
 from moveit_msgs.msg import DisplayTrajectory, RobotState, PlanningScene, CollisionObject
 from geometry_msgs.msg import PoseStamped
 from shape_msgs.msg import SolidPrimitive
+from sensor_msgs.msg import JointState
 
 import tf
 

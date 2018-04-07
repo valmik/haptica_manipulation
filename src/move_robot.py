ac33c229f7741b8d548895b567e86375d546bda7
valmik <rsnorunt@gmail.com>
2018-04-07 06:30:08 -0700

    start move_robot.py

---
+++ src/move_robot.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+
+import rospy
+import moveit_commander
+from moveit_msgs.msg import DisplayTrajectory, RobotState, PlanningScene, CollisionObject
+from geometry_msgs.msg import PoseStamped
+from shape_msgs.msg import SolidPrimitive
+
+import tf
+
+import sys
+import numpy as np
+
+
+
+class KinovaController(object):
+    def __init__(self):
+        """
+        Robot controller class.
+        Referenced dynamicreplanning.weebly.com and moveit python interface tutorial
+        """
+        rospy.loginfo("To stop project CTRL + C")
+        rospy.on_shutdown(self.shutdown)
+
+        # Initialize moveit_commander
+        moveit_commander.roscpp_initialize(sys.argv)
+
+        # Instatiate the move group
+        self.group = moveit_commander.MoveGroupCommander('arm')
+        self.group.set_planning_time(5)
+
+        # This publishes trajectories to RVIZ for visualization
+        self.display_planned_path_publisher = rospy.Publisher('arm/display_planned_path', DisplayTrajectory, queue_size=10)
+        
+        rate = rospy.Rate(10)
+
+    def shutdown(self):
+        rospy.loginfo("Stopping project")
+        rospy.sleep(1)
+
+    def collision_free_move(self, end_state):
+        """
+        Uses MoveIt to plan a path from the current state to end_state and returns it
+        end_state: list of joint values
+        """
+
+        self.group.set_start_state_to_current_state()
+        self.group.set_joint_value_target(end_state)
+
+        self.group.set_workspace([-3, -3, -3, 3, 3, 3])
+        
+        # Plan the path
+        plan = self.group.plan()
+
+        return plan
+
+    def visualize_plan(self, plan):
+        """
+        Visualize the plan in RViz
+        """
+
+        display_trajectory = DisplayTrajectory()
+        display_trajectory.trajectory_start = plan.points[0]
+        display_trajectory.trajectory.extend(plan.points)
+        self.display_planned_path_publisher.publish(display_trajectory)
+
+def make_pose(position, orientation, frame):
+    """
+    Creates a PoseStamped message based on provided position and orientation
+    position: numpy array of size 3
+    orientation: numpy array of size 4 (quaternion)
+    frame: string (the reference frame to which the pose is defined)
+    """
+
+    pose = PoseStamped()
+    pose.header.frame_id = frame
+    pose.pose.position.x = position[0]
+    pose.pose.position.y = position[1]
+    pose.pose.position.z = position[2]
+    pose.pose.orientation.w = orientation[0]
+    pose.pose.orientation.x = orientation[1]
+    pose.pose.orientation.y = orientation[2]
+    pose.pose.orientation.z = orientation[3]
+    return pose
+
+def add_arbitrary_obstacle(size, id, pose, planning_scene_publisher, scene, robot, operation):
+    """
+    Adds an arbitrary rectangular prism obstacle to the planning scene.
+    This is currently only for the ground plane
+    size: numpy array of size 3 (x,y,z dimensions)
+    id: string (object id/name)
+    pose: PoseStamped objecct (objects pose with respect to world frame)
+    planning_scene_publisher: ros Publisher('/collision_object', CollisionObject)
+    scene: PlanningSceneInterface
+    robot: RobotCommander
+    operation: currently just use CollisionObject.ADD
+    """
+
+    co = CollisionObject()
+    co.operation = operation
+    co.id = id
+    co.header = pose.header
+    box = SolidPrimitive()
+    box.type = SolidPrimitive.BOX
+    box.dimensions = size
+    co.primitives = [box]
+    co.primitive_poses = [pose.pose]
+    planning_scene_publisher.publish(co)
+
+
+
+if __name__ == '__main__':
+    rospy.init_node('kinova_controller')
+
+    # if len(sys.argv) > 1:
+    #     filename = sys.argv[1]
+
+    joints1 = [0, 0, 0, 0, 0, 0]
+    joints2 = [0.0, 2.9, 1.3, 4.2, 1.4, 0.0]
+
+
+    planning_scene_publisher = rospy.Publisher('/collision_object', CollisionObject)
+
+    scene = moveit_commander.PlanningSceneInterface()
+    robot = moveit_commander.RobotCommander()
+    rospy.sleep(2)
+
+    kinova_controller = KinovaController()
+
+    plan = kinova_controller.collision_free_move(joints1)
+
+
+
+
+
+
+
+
